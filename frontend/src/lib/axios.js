b93621b5b965e0afccc215e39b40e9e9eaba4e79
mfaaizalam <mfaaiz2000bce@gmail.com>
2026-02-26 15:34:46 +0500

    deploy

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL:"https://portfolio-theta-seven-33.vercel.app/api",
+    baseURL:"https://portfolio-git-main-muhammad-faaiz-alams-projects.vercel.app/api",
     // baseURL:"http://localhost:5000/api",
 })
