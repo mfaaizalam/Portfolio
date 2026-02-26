995a71f5eeff46f12a18737fffd928bb4ffb9561
mfaaizalam <mfaaiz2000bce@gmail.com>
2026-02-26 14:44:07 +0500

    deploying

--- backend/src/server.js
+++ backend/src/server.js
@@ -18,9 +18,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', ContactRoute);
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+
 app.get("/", (req, res) => res.send("Backend is running!"));
 // const PORT = process.env.PORT || 3000;
 
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL:"http://localhost:5000/api",
+    baseURL:"https://portfolio-theta-seven-33.vercel.app/api",
     
 })
